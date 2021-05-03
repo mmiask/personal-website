20128f55479bb2795f189317976a1caf6fab853a
mmiask <mmiask92@gmail.com>
2021-05-03 14:33:03 +0200

    swtich to CRA + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-blue-500">
+      siemanko
+    </div>
+  )
+}
+
+export default App;
